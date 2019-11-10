d329ca04836a5ad3b41926b8c6ef0fb3b1e52a36
Alexander Nesterov <nesoldr@mail.ru>
2019-11-10 20:42:58 -0100

    Added getArg() for my new tests

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/NesterovAlexanderTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/NesterovAlexanderTest.java
@@ -6,33 +6,6 @@ import static org.junit.Assert.*;
 
 public class NesterovAlexanderTest {
     @Test
-    public void isRightNormComplexNumer() {
-        // Arrange
-        ComplexNumber z1 = new ComplexNumber(2, 6);
-        ComplexNumber z1Inv = new ComplexNumber(2, -6);
-        double normZ = Math.pow(2, 2) + Math.pow(6, 2);
-
-        // Act
-        ComplexNumber z = z1.multiply(z1Inv);
-
-        // Assert
-        assertEquals(new ComplexNumber(normZ, 0), z);
-    }
-
-    @Test
-    public void isRightRelativeMathRelation() {
-        // Arrange
-        ComplexNumber z1 = new ComplexNumber(2.22, 4.67);
-        ComplexNumber z1Inv = new ComplexNumber(2.22, -4.67);
-
-        // Act
-        ComplexNumber z = z1.add(z1Inv);
-
-        // Assert
-        assertEquals(4.44, z.getReal() + z.getImaginary(), 1e-10);
-    }
-
-    @Test
     public void canArgNumberHalfPI() {
         // Arrange
         ComplexNumber z = new ComplexNumber(0, 1);
@@ -66,19 +39,5 @@ public class NesterovAlexanderTest {
 
         // Assert
         assertEquals(res, Math.PI / 4, 1e-15);
-    }
-
-    @Test
-    public void canWorkWithArgMathRelations() {
-        // Arrange
-        ComplexNumber z1 = new ComplexNumber(5.78, 3.87);
-        ComplexNumber z2 = new ComplexNumber(9.34, -0.87);
-
-        // Act
-        double res1 = (z1.multiply(z2)).getArg();
-        double res2 = z1.getArg() + z2.getArg();
-
-        // Assert
-        assertEquals(res1, res2, 1e-10);
     }
 }
