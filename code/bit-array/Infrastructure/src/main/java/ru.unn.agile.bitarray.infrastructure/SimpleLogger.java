447e6b4fe197ff82f070fad16de22ea8e998226f
Rostislav Povelikin <povelikin.rostislav@gmail.com>
2019-12-25 13:21:11 +0300

    Add opening of the file to write on creation

--- code/bit-array/Infrastructure/src/main/java/ru.unn.agile.bitarray.infrastructure/SimpleLogger.java
+++ code/bit-array/Infrastructure/src/main/java/ru.unn.agile.bitarray.infrastructure/SimpleLogger.java
@@ -2,10 +2,17 @@ package ru.unn.agile.bitarray.infrastructure;
 
 import ru.unn.agile.bitarray.viewmodel.LoggerInterface;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.List;
 
 public class SimpleLogger implements LoggerInterface {
     public SimpleLogger(final String logFileName) {
+        try {
+            new BufferedWriter(new FileWriter(logFileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- code/bit-array/Infrastructure/src/test/java/ru/unn/agile/bitarray/infrastructure/SimpleLoggerTests.java
+++ code/bit-array/Infrastructure/src/test/java/ru/unn/agile/bitarray/infrastructure/SimpleLoggerTests.java
@@ -2,6 +2,11 @@ package ru.unn.agile.bitarray.infrastructure;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import static org.junit.Assert.*;
 
 public class SimpleLoggerTests {
@@ -17,4 +22,14 @@ public class SimpleLoggerTests {
     public void canConstruct() {
         assertNotNull(logger);
     }
+
+    @Test
+    public void loggerCreatesRealLogFile() {
+        try {
+            new BufferedReader(new FileReader(LOG_FILE));
+        } catch (FileNotFoundException e) {
+            fail(LOG_FILE + " must be created by the logger in ctor.");
+        }
+    }
+
 }
