31e73d266648f6eaaa79206c788f889c2498775f
unknown <mari.louis.von.franz@gmail.com>
2020-01-14 12:33:34 +0300

    real logger is completed

--- code/numbers-to-words/Infrastructure/src/main/java/ru/unn/agile/numberstowords/Infrastructure/TxtLogger.java
+++ code/numbers-to-words/Infrastructure/src/main/java/ru/unn/agile/numberstowords/Infrastructure/TxtLogger.java
@@ -2,22 +2,64 @@ package ru.unn.agile.numberstowords.Infrastructure;
 
 import ru.unn.agile.numberstowords.ViewModel.ILogger;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class TxtLogger implements ILogger {
-    private final String filename;
+    private static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+    private BufferedWriter writer = null;
+    private final String fileName;
 
+    public TxtLogger(final String fileName) {
+        this.fileName = fileName;
 
-    public TxtLogger(final String filename) {
-        this.filename = filename;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getDateNow() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW, Locale.ENGLISH);
+        return sdf.format(calendar.getTime());
     }
 
     @Override
     public void log(final String s) {
+        try {
+            writer.write(getDateNow() + " :\t" + s);
+            writer.newLine();
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public List<String> getLog() {
-        return null;
+        BufferedReader reader;
+        ArrayList<String> log = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+
+            while (line != null) {
+                log.add(line);
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return log;
     }
 }
--- code/numbers-to-words/Infrastructure/src/test/java/ru/unn/agile/numberstowords/infrastructure/TxtLoggerTests.java
+++ code/numbers-to-words/Infrastructure/src/test/java/ru/unn/agile/numberstowords/infrastructure/TxtLoggerTests.java
@@ -3,7 +3,13 @@ package ru.unn.agile.numberstowords.Infrastructure;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.*;
 
 public class TxtLoggerTests {
     private static final String FILENAME = "./TxtLoggerTests.log";
@@ -17,5 +23,54 @@ public class TxtLoggerTests {
     @Test
     public void canCreateLoggerWithFileName() {
         assertNotNull(txtLogger);
+    }
+
+    @Test
+    public void canCreateLogFile() {
+        try {
+            new BufferedReader(new FileReader(FILENAME));
+        } catch (FileNotFoundException e) {
+            fail("File " + FILENAME + " was not found!");
+        }
+    }
+
+    @Test
+    public void logFileIsEmptyInTheBeginning() {
+        List<String> log = txtLogger.getLog();
+
+        assertEquals(0, log.size());
+    }
+
+    @Test
+    public void canAddLogMessage() {
+        String testMessage = "Test message";
+
+        txtLogger.log(testMessage);
+
+        String message = txtLogger.getLog().get(0);
+        assertTrue(message.matches(".*" + testMessage + "$"));
+    }
+
+    @Test
+    public void logMessageContainsDateAndTimeInformation() {
+        String testMessage = "Test message";
+
+        txtLogger.log(testMessage);
+
+        String message = txtLogger.getLog().get(0);
+        assertTrue(message.matches(".*\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} :\t.*"));
+    }
+
+    @Test
+    public void canAddSeveralLogMessages() {
+        String[] testMessages = {"Test message 1", "Test message 2"};
+
+        txtLogger.log(testMessages[0]);
+        txtLogger.log(testMessages[1]);
+
+        List<String> log = txtLogger.getLog();
+        for (int i = 0; i < log.size(); i++) {
+            assertTrue(log.get(i).matches(".*" + testMessages[i] + ".*"));
+        }
     }
 }
