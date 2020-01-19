7445dde22270d9ba1860dd55693476e00e3d5a8c
Alexander Nesterov <nesoldr@mail.ru>
2020-01-19 15:50:26 -0100

    added view

--- code/red-black-tree/View/src/main/java/ru/unn/agile/redblacktree/view/RedBlackTreeView.java
+++ code/red-black-tree/View/src/main/java/ru/unn/agile/redblacktree/view/RedBlackTreeView.java
@@ -1,4 +1,65 @@
 package ru.unn.agile.redblacktree.view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import ru.unn.agile.redblacktree.viewmodel.ViewModel;
+
 public class RedBlackTreeView {
+    @FXML
+    private ViewModel viewModel;
+    @FXML
+    private TextField txtAddField;
+    @FXML
+    private TextField txtFindField;
+    @FXML
+    private TextField txtRemoveField;
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnFind;
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private Label txtResultFind;
+    @FXML
+    private Label txtResultRemove;
+
+    @FXML
+    void initialize() {
+
+        txtAddField.textProperty().bindBidirectional(viewModel.addFieldProperty());
+        txtFindField.textProperty().bindBidirectional(viewModel.findInsertFieldProperty());
+        txtRemoveField.textProperty().bindBidirectional(viewModel.removeInsertFieldProperty());
+        txtAddField.textProperty().bindBidirectional(viewModel.addFieldProperty());
+
+        txtResultFind.textProperty().bindBidirectional(viewModel.resultFindProperty());
+        txtResultRemove.textProperty().bindBidirectional(viewModel.resultRemoveProperty());
+
+        btnAdd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
+                viewModel.addElementToTree();
+            }
+        });
+
+        btnFind.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
+                viewModel.findElementToTree();
+            }
+        });
+
+        btnRemove.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
+                viewModel.removeElementToTree();
+            }
+        });
+    }
 }
