239d12d397587303d9bb1acac27abb92104b5dcd
Alexander Nesterov <nesoldr@mail.ru>
2020-01-19 14:14:09 -0100

    Added field add

--- code/red-black-tree/ViewModel/src/main/java/ru/unn/agile/redblacktree/viewmodel/ViewModel.java
+++ code/red-black-tree/ViewModel/src/main/java/ru/unn/agile/redblacktree/viewmodel/ViewModel.java
@@ -1,9 +1,17 @@
 package ru.unn.agile.redblacktree.viewmodel;
 
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import ru.unn.agile.redblacktree.model.RedBlackTree;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewModel {
     private final StringProperty addField = new SimpleStringProperty();
@@ -16,6 +24,11 @@ public class ViewModel {
     private final StringProperty removeResultField = new SimpleStringProperty();
     private RedBlackTree rbTree = new RedBlackTree();
 
+    private final BooleanProperty addElementToTreeDisabled = new SimpleBooleanProperty();
+
+    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
+    private final StringProperty status = new SimpleStringProperty();
+
     public ViewModel() {
         addField.set("");
         isEmptyField.set("");
@@ -25,6 +38,32 @@ public class ViewModel {
         findResultField.set("");
         removeInsertField.set("");
         removeResultField.set("");
+
+        status.set(Status.WAITING.toString());
+
+        BooleanBinding couldAddElementToTree = new BooleanBinding() {
+            {
+                super.bind(addField, findInsertField, removeInsertField);
+            }
+            @Override
+            protected boolean computeValue() {
+                return getInputStatus() == Status.READY;
+            }
+        };
+        addElementToTreeDisabled.bind(couldAddElementToTree.not());
+
+        // Add listeners to the input text fields
+        final List<StringProperty> fields = new ArrayList<StringProperty>() { {
+            add(addField);
+            add(findInsertField);
+            add(removeInsertField);
+        } };
+
+        for (StringProperty field : fields) {
+            final ValueChangeListener listener = new ValueChangeListener();
+            field.addListener(listener);
+            valueChangedListeners.add(listener);
+        }
     }
 
     public StringProperty addFieldProperty() {
@@ -50,5 +89,66 @@ public class ViewModel {
     }
     public StringProperty removeResultFieldProperty() {
         return removeResultField;
+    }
+
+    public StringProperty statusProperty() {
+        return status;
+    }
+    public final String getStatus() {
+        return status.get();
+    }
+
+    private Status getInputStatus() {
+        Status inputStatus = Status.READY;
+        if (addField.get().isEmpty() ||
+                findInsertField.get().isEmpty() ||
+                removeInsertField.get().isEmpty()) {
+            inputStatus = Status.WAITING;
+        }
+        try {
+            if (!addField.get().isEmpty()) {
+                Integer.parseInt(addField.get());
+            }
+            if (!findInsertField.get().isEmpty()) {
+                Integer.parseInt(findInsertField.get());
+            }
+            if (!removeInsertField.get().isEmpty()) {
+                Integer.parseInt(removeInsertField.get());
+            }
+        } catch (NumberFormatException nfe) {
+            inputStatus = Status.BAD_FORMAT;
+        }
+        return inputStatus;
+    }
+
+    public void addElementToTree() {
+        if (addElementToTreeDisabled.get()) {
+            return;
+        }
+        rbTree.insert(Integer.parseInt(addField.get()));
+        status.set(Status.SUCCESS.toString());
+    }
+
+    private class ValueChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(final ObservableValue<? extends String> observable,
+                            final String oldValue, final String newValue) {
+            status.set(getInputStatus().toString());
+        }
+    }
+}
+
+enum Status {
+    WAITING("Please provide input data"),
+    READY("Press button"),
+    BAD_FORMAT("Bad format"),
+    SUCCESS("Success");
+
+    private final String name;
+    Status(final String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return name;
     }
 }
--- code/red-black-tree/ViewModel/src/test/java/ru/unn/agile/redblacktree/viewmodel/ViewModelTest.java
+++ code/red-black-tree/ViewModel/src/test/java/ru/unn/agile/redblacktree/viewmodel/ViewModelTest.java
@@ -21,12 +21,14 @@ public class ViewModelTest {
     @Test
     public void canSetDefaultValues() {
         assertEquals("", viewModel.addFieldProperty().get());
-        assertEquals("", viewModel.isEmptyFieldProperty().get());
-        assertEquals("", viewModel.getSizeFieldProperty().get());
-        assertEquals("", viewModel.minimumFieldProperty().get());
         assertEquals("", viewModel.findInsertFieldProperty().get());
-        assertEquals("", viewModel.findResultFieldProperty().get());
         assertEquals("", viewModel.removeInsertFieldProperty().get());
-        assertEquals("", viewModel.removeResultFieldProperty().get());
+    }
+
+
+    @Test
+    public void statusIsWaitingWhenAddElementToTreeWithEmptyFields() {
+        viewModel.addElementToTree();
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
     }
 }
