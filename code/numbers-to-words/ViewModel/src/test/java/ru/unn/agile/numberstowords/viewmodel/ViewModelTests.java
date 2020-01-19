597472c00ee9d54225c161fceb6ddb0d138657d5
Alexander Nesterov <nesoldr@mail.ru>
2020-01-19 16:09:09 -0100

    added changes because CI - red - 2

--- code/numbers-to-words/ViewModel/src/test/java/ru/unn/agile/numberstowords/viewmodel/ViewModelTests.java
+++ code/numbers-to-words/ViewModel/src/test/java/ru/unn/agile/numberstowords/viewmodel/ViewModelTests.java
@@ -3,6 +3,7 @@ package ru.unn.agile.numberstowords.viewmodel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ViewModelTests {
     private ViewModel viewModelForm;
